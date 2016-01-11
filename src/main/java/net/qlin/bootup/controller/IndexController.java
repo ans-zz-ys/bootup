15492d2135c1ae488f21cb354aec73fafc3a144a
qiulin <xcodeman@gmail.com>
2016-01-11 17:37:44 +0800

    update

---
+++ src/main/java/net/qlin/bootup/controller/IndexController.java
@@ -0,0 +1,15 @@
+package net.qlin.bootup.controller;
+
+/**
+ * Created by qiulin on 2016/1/11.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class IndexController {
+}
