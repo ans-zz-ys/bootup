18429b1d3e33ca040db45eacc04ca94befc06642
qiulin <xcodeman@gmail.com>
2016-01-11 08:24:45 +0100

    fix typo

--- src/main/java/net/qlin/bootup/BootupApplication.java
+++ src/main/java/net/qlin/bootup/BootupApplication.java
@@ -1,12 +1,18 @@
 package net.qlin.bootup;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootupApplication {
+public class BootUpApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootupApplication.class, args);
+		SpringApplication.run(BootUpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BootUp is boot up...");
 	}
 }
