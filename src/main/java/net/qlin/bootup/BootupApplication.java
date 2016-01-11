54084b574a39ad8a60ac11087ad2b53ca35f626b
qiulin <xcodeman@gmail.com>
2016-01-11 07:59:21 +0100

    init repo

---
+++ src/main/java/net/qlin/bootup/BootupApplication.java
@@ -0,0 +1,12 @@
+package net.qlin.bootup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootupApplication.class, args);
+	}
+}
